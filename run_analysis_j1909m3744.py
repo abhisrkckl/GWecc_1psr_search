e1d179ee26a7568446bbc125b4e0d31207919ea6
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-03-14 12:29:30 -0500

    run sampler

--- run_analysis_j1909m3744.py
+++ run_analysis_j1909m3744.py
@@ -1,5 +1,6 @@
 from analysis import read_data, get_pta, get_deltap_max
 from enterprise.signals.parameter import Uniform
+from PTMCMCSampler.PTMCMCSampler import PTSampler as ptmcmc
 
 import numpy as np
 
@@ -17,15 +18,15 @@ def main():
     priors = {
         "sigma": Uniform(0, np.pi)(f"{name}_sigma"),
         "rho": Uniform(-np.pi, np.pi)(f"{name}_rho"),
-        "log10_M": Uniform(6, 9)(f"{name}_log10_M"), 
-        "eta": Uniform(0, 0.25)(f"{name}_eta"),
-        "log10_F": Uniform(-9, -7)(f"{name}_log10_F"),
-        "e0": Uniform(0.01, 0.8)(f"{name}_e0"),
-        "l0": Uniform(-np.pi, np.pi)(f"{name}_l0"),
+        "log10_M": 9.0, # Uniform(6, 9)(f"{name}_log10_M"), 
+        "eta": 0.25, # Uniform(0, 0.25)(f"{name}_eta"),
+        "log10_F": -8, # Uniform(-9, -7)(f"{name}_log10_F"),
+        "e0": 0.5, # Uniform(0.01, 0.8)(f"{name}_e0"),
+        "l0": 0.0, # Uniform(-np.pi, np.pi)(f"{name}_l0"),
         "tref": tref,
         "log10_A": Uniform(-11, -5)(f"{name}_log10_A"),
         "deltap": Uniform(0, deltap_max),
-        "psrTerm": True,
+        "psrTerm": False,
         "spline": True,
     }
 
@@ -36,6 +37,29 @@ def main():
     # This also triggers JIT compilation of julia code and the caching of ENTERPRISE computations.
     x0 = np.array([p.sample() for p in pta.params])
     print("Log-likelihood at", x0, "is", pta.get_lnlikelihood(x0))
+
+    ndim = len(x0)
+    cov = np.diag(np.ones(ndim) * 0.01**2)
+    Niter = 500000
+    x0 = np.hstack(x0)
+    sampler = ptmcmc(
+        ndim,
+        pta.get_lnlikelihood,
+        pta.get_lnprior,
+        cov,
+        outDir="chains/",
+        resume=False,
+    )
+    # This sometimes fails if the acor package is installed, but works otherwise.
+    # I don't know why.
+    sampler.sample(x0, Niter)
+
+    chain_file = "chains/chain_1.txt"
+    chain = np.loadtxt(chain_file)
+    print("Chain shape :", chain.shape)
+
+    burn = chain.shape[0] // 3
+    burned_chain = chain[burn:, :-4]
 
 
 if __name__ == "__main__":
