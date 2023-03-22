496b9ebbf8bc07e79f4926ad966d9d3bc4d785dc
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-03-22 16:17:54 -0500

    plot upper limit

--- run_1psr_analysis.py
+++ run_1psr_analysis.py
@@ -116,8 +116,69 @@ def main():
     corner.corner(burned_chain, labels=pta.param_names)
     plt.savefig(f"{outdir}/corner.pdf")
 
+    if not settings["noise_only"]:
+        plot_upper_limit(
+            pta.param_names, 
+            burned_chain, 
+            "gwecc_log10_F", 
+            "$\\log_{10} f_{gw}$ (Hz)", 
+            (-9,-7), 
+            xparam_bins=16, 
+            quantile=0.95
+        )
+        plt.savefig(f"{outdir}/upper_limit_freq.pdf")
+
+        plot_upper_limit(
+            pta.param_names, 
+            burned_chain, 
+            "gwecc_log10_M", 
+            "$\\log_{10} M$ (Msun)", 
+            (6,9), 
+            xparam_bins=8, 
+            quantile=0.95
+        )
+        plt.savefig(f"{outdir}/upper_limit_mass.pdf")
+
+        plot_upper_limit(
+            pta.param_names, 
+            burned_chain, 
+            "gwecc_e0", 
+            "$e_0$", 
+            (0.01,0.8), 
+            xparam_bins=8, 
+            quantile=0.95
+        )
+        plt.savefig(f"{outdir}/upper_limit_ecc.pdf")
+
     print("Done.")
 
+
+def plot_upper_limit(param_names, chain, xparam_name, xparam_label, xparam_lims, xparam_bins=16, quantile=0.95):
+    ampl_idx = param_names.index("gwecc_log10_A")
+    freq_idx = param_names.index(xparam_name)
+
+    chain_ampl = chain[:,ampl_idx]
+    chain_freq = chain[:,freq_idx]
+
+    log10_F_min, log10_F_max = xparam_lims
+    log10_F_lins = np.linspace(log10_F_min, log10_F_max, xparam_bins+1)
+    log10_F_mins = log10_F_lins[:-1]
+    log10_F_maxs = log10_F_lins[1:]
+    log10_F_mids = (log10_F_mins + log10_F_maxs)/2
+
+    log10_A_quant_Fbin = []
+    for log10_fmin, log10_fmax in zip(log10_F_mins, log10_F_maxs):
+        freq_mask = np.logical_and(chain_freq>=log10_fmin, chain_freq<log10_fmax)
+        log10_A_Fbin_data = chain_ampl[freq_mask]
+        log10_A_quant = np.quantile(log10_A_Fbin_data, quantile)
+        log10_A_quant_Fbin.append(log10_A_quant)
+
+    log10_A_quant = np.quantile(chain_ampl, quantile)
+
+    plt.plot(log10_F_mids, log10_A_quant_Fbin)
+    plt.axhline(log10_A_quant, color="grey")
+    plt.xlabel(xparam_label)
+    plt.ylabel(f"{int(100*quantile)}%" + " upper bound on $\\log_{10} \\zeta_0$ (s)")
 
 if __name__ == "__main__":
     main()
